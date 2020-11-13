35bd10b5c2e02fad3767cd0cb69808d6eca6eacd
narumol01 <73689289+narumol01@users.noreply.github.com>
2020-11-13 15:57:52 +0700

    Update lab13.php

--- lab13.php
+++ lab13.php
@@ -1,38 +1,26 @@
-<html>
-<head>
-<title>ITE lab</title>
-</head>
-<body>
 <?php
+
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'lab-itf-13.mysql.database.azure.com', 'it63070094@lab-itf-13', 'ITFLab13', 3306);
+mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
 ?>
-<table width="600" border="1">
-    <tr>
-        <th width="100"> <div align="center">Name</div></th>
-        <th width="150"> <div align="center">Link </div></th>
-        <th width="350"> <div align="center">Comment</div></th>
-    </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-    <tr>
-        <td><?php echo $Result['Name'];?></div></td>
-        <td><?php echo $Result['Link'];?></td>
-        <td><?php echo $Result['Comment'];?></td>
-    </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysquli_close($conn);
-?>
-</body>
-</html>
